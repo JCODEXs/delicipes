a05f95416d61009bef3e71aa7e735252a0a7bece
JCODEXs <juansebastianescobar.vega@gmail.com>
2024-05-08 11:42:49 -0500

    separateQuery

--- src/_components/topnav.tsx
+++ src/_components/topnav.tsx
@@ -1,14 +1,24 @@
+"use client";
 import { SignedIn, SignInButton, SignedOut, UserButton } from "@clerk/nextjs";
+import { UploadButton } from "~/utils/uploadthings";
+import { useRouter } from "next/navigation";
 
 export default function TopNav() {
+  const router = useRouter();
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
       <div>Gallery</div>
-      <div>
+      <div className="flex flex-row">
         <SignedOut>
-          <SignInButton />
+          <SignInButton mode="modal" />
         </SignedOut>
         <SignedIn>
+          <UploadButton
+            endpoint="imageUploader"
+            onClientUploadComplete={() => {
+              router.refresh();
+            }}
+          />
           <UserButton />
         </SignedIn>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,43 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
+import Image from "next/image";
 import Link from "next/link";
 import { db } from "~/server/db";
 export const dynamic = "force-dynamic";
-const mockUrl = [
-  "https://utfs.io/f/3849b83f-40f9-428e-8b2e-2efaa0f2b6ce-b280y9.jpg",
-  "https://utfs.io/f/0030dba0-fa65-49b0-952d-7a971d79ce3b-3lq3q2.png",
-  "https://utfs.io/f/77ca9942-7803-480f-ac74-21b4ea9d90b9-orx84a.bmp",
-  "https://utfs.io/f/71574bcc-ac9c-4176-8f67-f5c518262a03-1xa0my.jpeg",
-];
+import { getMyImages } from "~/server/queries";
+async function Images() {
+  const images = await getMyImages();
 
-const MockImages = mockUrl.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
+  return (
+    <div className="flex flex-wrap justify-center gap-4 p-4">
+      {images.map((image) => (
+        <div key={image.id} className="flex h-48 w-48 flex-col">
+          <Link href={`/img/${image.id}`}>
+            <img
+              src={image.url}
+              style={{ objectFit: "contain" }}
+              width={192}
+              height={192}
+              alt={image.name}
+            />
+          </Link>
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export default async function HomePage() {
-  const images = await db.query.images.findMany({
-    orderBy: (model, { desc }) => desc(model.id),
-  });
-  console.log(images);
   return (
     <main className="flex min-h-screen flex-col items-center justify-center  text-white">
-      <div className=" flex flex-wrap gap-4">
-        {/* {images.map((post) => (
-          <div key={post.id}>{post.name}</div>
-        ))} */}
-        {images.map((image, index) => (
-          <div key={image.id + "-" + index} className="flex w-48 flex-col">
-            <img src={image.url} alt="image" height={160} width={160} />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">
+          Please sign in above
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
