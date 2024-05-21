315b7d4b3a6006473abc9e1be3348a426db15055
JCODEXs <juansebastianescobar.vega@gmail.com>
2024-05-21 10:26:13 -0500

    ratelimiter

--- src/app/_components/simple-upload-button.tsx
+++ src/app/_components/simple-upload-button.tsx
@@ -59,7 +59,7 @@ function LoadingSpinnerSVG() {
       height="24"
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
-      fill="white"
+      fill="black"
     >
       <path
         d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z"
@@ -83,7 +83,8 @@ export function SimpleUploadButton() {
       posthog.capture("upload_begin");
       toast(
         <div className="flex items-center gap-2 text-white">
-          <LoadingSpinnerSVG /> <span className="text-lg">Uploading...</span>
+          <LoadingSpinnerSVG />
+          <span className="text-lg  text-black">Uploading...</span>
         </div>,
         {
           duration: 100000,
