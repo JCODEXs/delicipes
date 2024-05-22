f97c55cf9f08c5d972e6bcc1d8965361dc5b9173
JCODEXs <juansebastianescobar.vega@gmail.com>
2024-05-22 12:49:00 -0500

    weddingCake

--- src/components/reception-page.tsx
+++ src/components/reception-page.tsx
@@ -17,21 +17,21 @@ export default async function FullPageRecetionView() {
             className="w-full max-w-md object-contain"
           />
         </div>
-      </div>
-      <div className="flex w-full flex-1 flex-col border-t bg-gray-800 text-white lg:w-1/3 lg:border-l lg:border-t-0">
-        <div className="p-2">
-          <div>Estamos muy felices de tenerlos con nosotros</div>
-        </div>
-        <div className="p-2">
-          <div className="flex items-center text-xs">
-            Esta aplicacion fue creada para que nos dejen las fotos de la
-            reunion usando el boton
-            <span className="ml-1 inline-flex items-center">
-              <UploadSVG />
-            </span>
+        <div className="flex w-full flex-1 flex-col border-t bg-gray-800 text-white lg:w-1/3 lg:border-l lg:border-t-0">
+          <div className="p-2">
+            <div>Estamos muy felices de tenerlos con nosotros</div>
           </div>
+          <div className="p-2">
+            <div className="flex items-center text-xs">
+              Esta aplicacion fue creada para que nos dejen las fotos de la
+              reunion usando el boton
+              <span className="ml-1 inline-flex items-center">
+                <UploadSVG />
+              </span>
+            </div>
+          </div>
+          <div className="p-2"></div>
         </div>
-        <div className="p-2"></div>
       </div>
     </div>
   );
