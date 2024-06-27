6ece56fea1f9aa68a93c32c6c31b9aafb0d4ce0b
JCODEXs <juansebastianescobar.vega@gmail.com>
2024-06-26 22:46:54 -0500

    wip

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,7 +6,7 @@ export default {
   schema: "./src/server/db/schema.ts",
   driver: "pg",
   dbCredentials: {
-    connectionString: env.POSTGRES_URL,
+    connectionString: "postgres://default:M7G0psCaVoQO@ep-cold-shadow-a4tzsac7-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require",
   },
   tablesFilter: ["dashboard_*"],
 } satisfies Config;
